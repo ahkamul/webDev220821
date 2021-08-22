490719b58f5218aeade6acd56681bbd4fb3ba518
ahkamul <sasraishougat@gmail.com>
2021-08-22 18:19:02 +0600

    Code on JS Math Object, Random Number, Booleans and Conditional & Logical Operator written in index.js

---
+++ index.js
@@ -0,0 +1,156 @@
+// JS Math Object :
+
+// Math.round()
+
+// console.log(Math.round(4.9))
+// console.log(Math.round(4.7))
+// console.log(Math.round(4.4))
+// console.log(Math.round(4.2))
+// console.log(Math.round(- 4.2))
+
+// Math.ceil()
+
+// console.log(Math.ceil(4.9))
+// console.log(Math.ceil(4.7))
+// console.log(Math.ceil(4.4))
+// console.log(Math.ceil(4.2))
+// console.log(Math.ceil(- 4.2))
+
+
+// Math.floor()
+
+// console.log(Math.floor(4.9))
+// console.log(Math.floor(4.7))
+// console.log(Math.floor(4.4))
+// console.log(Math.floor(4.2))
+// console.log(Math.floor(- 4.2))
+
+// Math.trunc()
+
+// console.log(Math.trunc(4.9))
+// console.log(Math.trunc(4.7))
+// console.log(Math.trunc(4.4))
+// console.log(Math.trunc(4.2))
+// console.log(Math.trunc(- 4.2))
+
+// Math.sign()
+
+// console.log(Math.sign(-4))
+// console.log(Math.sign(0))
+// console.log(Math.sign(4))
+// console.log(Math.sign(9))
+
+// Math.pow(x,y)
+
+// console.log(Math.pow(8, 2))
+// console.log(Math.pow(3, 4))
+
+// Math.sqrt()
+
+// console.log(Math.sqrt(64))
+// console.log(Math.sqrt(144))
+
+// Math.abs(x)
+
+// console.log(Math.abs(-4.7))
+
+// Math.sin()
+
+// console.log(Math.sin(90 * Math.PI / 180))
+// console.log(Math.sin(0 * Math.PI / 180))
+// console.log(Math.cos(90 * Math.PI / 180))
+// console.log(Math.cos(0 * Math.PI / 180))
+
+// Math.min() & Math.max()
+
+// console.log(Math.min(0, 150, 30, 20, -8, -200))
+// console.log(Math.max(0, 150, 30, 20, -8, -200))
+
+// Math.random
+
+// console.log(Math.random())
+
+// Math.log(x)
+
+// console.log(Math.log(1))
+// console.log(Math.log2(8))
+// console.log(Math.log10(1000))
+// console.log(Math.log10(100))
+
+
+// JS Random Number :
+
+// console.log(Math.random())
+// console.log(Math.random() * 10)
+// console.log(Math.random() * 11)
+// console.log(Math.random() * 100)
+
+// console.log(Math.floor(Math.random() * 10))
+// console.log(Math.floor(Math.random() * 11))
+// console.log(Math.floor(Math.random() * 100))
+
+// console.log(Math.floor(Math.random() * 100) + 1)
+
+// JS Random Function 
+
+// function getRndInteger (min, max){
+//     return Math.floor(Math.random() * (max - min) + min)   // Yet to know how it work 
+// }
+
+
+// JS Boolean :
+
+//console.log(Boolean(9 > 7))
+// console.log(9 > 7)
+// console.log(9 < 7)
+
+// More study needed to get js boolean 
+
+
+// JS Comparison Operator and Logical Operator :
+
+// var x = 5 
+
+// console.log(x == 8)
+// console.log(x == 5)
+// console.log(x == "5")
+
+// console.log(x === 5)
+// console.log(x === "5")
+
+// console.log(x != 8)   // != Not equal 
+// console.log(x != 5)
+
+// console.log(x !== 5)    // Not equal value or not equal type 
+// console.log(x !== "5")
+// console.log(x !== 8) 
+
+// console.log(x > 8)
+// console.log(x < 8)
+// console.log(x >= 8)
+// console.log(x <= 8) 
+
+// var x = 6; y = 3;
+
+// console.log(x + y)
+
+// console.log(x < 10 && y > 1)    // && - logical and
+// console.log(x < 10 && y < 1)  
+
+// console.log(x == 5 || y == 5)    // || - logical or 
+// console.log(x == 6 || y == 5)    
+
+// console.log (!(x == y))      // !== logical Not equal 
+
+
+// Comparing Different Types 
+
+// console.log(2 < 12)
+// console.log(2 < "12")
+// console.log(2 < "John")
+// console.log(2 > "John")
+// console.log(2 == "John")
+
+console.log("2" < "12")
+console.log("2" > "12")
+console.log("2" == "12")
